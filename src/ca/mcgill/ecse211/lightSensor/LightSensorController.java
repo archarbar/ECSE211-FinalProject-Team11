e2cf7850e377f2292162f0502f2de0d5eaa467ba
watem <matthew.williams5@mail.mcgill.ca>
2019-10-22 13:51:32 -0400

    lab5-V1.0

---
+++ src/ca/mcgill/ecse211/lightSensor/LightSensorController.java
@@ -0,0 +1,16 @@
+package ca.mcgill.ecse211.lightSensor;
+
+import lejos.hardware.sensor.EV3ColorSensor;
+
+public class LightSensorController {
+  enum edgeType {noEdge, rising, falling}
+  float currentLevel, previousLevel;
+  
+  
+  public LightSensorController(EV3ColorSensor sensor) {
+    
+  }
+  
+  
+
+}
