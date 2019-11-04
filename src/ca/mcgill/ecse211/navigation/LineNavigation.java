f689f41d88b73a884492a5996cdc7bb83345b9d1
watem <matthew.williams5@mail.mcgill.ca>
2019-11-04 17:59:43 -0500

    started travelTo()

--- src/ca/mcgill/ecse211/navigation/LineNavigation.java
+++ src/ca/mcgill/ecse211/navigation/LineNavigation.java
@@ -2,7 +2,9 @@ package ca.mcgill.ecse211.navigation;
 
 import static ca.mcgill.ecse211.project.Resources.*;
 import java.util.LinkedList;
+import ca.mcgill.ecse211.odometer.Odometer;
 import ca.mcgill.ecse211.odometer.OdometryCorrection;
+import lejos.robotics.SampleProvider;
 
 public class LineNavigation extends Navigation{
   private static boolean lineNavigating = false;
@@ -10,7 +12,7 @@ public class LineNavigation extends Navigation{
 
   
   
-  
+  /*
   public static void findLine() {
     lineNavigating = true;
     long startTime;
@@ -27,7 +29,7 @@ public class LineNavigation extends Navigation{
       leftMotor.rotate(angle, true);
       rightMotor.rotate(angle, true);
 
-      while (!OdometryCorrection.lineDetected(centreLightSensor)
+      while (!OdometryCorrection.lineDetected())
           || (System.currentTimeMillis() - startTime) / 1000 > searchTime * count) {
         try {
           Thread.sleep(CORRECTION_PERIOD);
@@ -44,6 +46,7 @@ public class LineNavigation extends Navigation{
     // if no line detected in small distance, move backwards
     // if still no line detected, expand a bit more
   }
+  */
 
   /**
    * @return the lineNavigating
@@ -59,7 +62,53 @@ public class LineNavigation extends Navigation{
     return turning;
   }
   
-  public static void travelTo() {}
+  public static void travelTo(int x, int y) {
+    double ax = (x+0.5)*TILE_SIZE;
+    double ay = (y+0.5)*TILE_SIZE;
+    travelTo(ax, ay);
+  }
+  
+  public static void travelTo(double x, double y) {
+    if (outOfBounds(x, y)) {
+      return;
+    }
+    lineNavigating = true;
+    
+    
+    double[] position = Odometer.getOdometer().getXYT();
+    double dx = x-position[0];
+    double dy = y-position[1];
+    double angle;
+    double distance;
+    
+    angle = angleToTarget(x, y);
+    angle = Math.round(angle/90)*90;
+    turnToHeading(angle);
+    if(Math.abs(dx)>Math.abs(dy)) {
+     
+    }
+    else {
+      
+    }
+    angle = angleToTarget(x, y);
+    distance = calculateDistanceTo(x, y);
+    turnTo(angle);
+    moveTo(distance);
+    lineNavigating = false;
+  }
 
+  private static double[] nearestIntersect() {
+    double position[] = Odometer.getOdometer().getXYT();
+    return nearestIntersect(position[0], position[1]);
+  }
 
+  private static double[] nearestIntersect(double x, double y) {
+    double intersect[] = new double[2];
+    double position[] = Odometer.getOdometer().getXYT();
+    intersect[0] = Math.round(x/TILE_SIZE)*TILE_SIZE;
+    intersect[1] = Math.round(y/TILE_SIZE)*TILE_SIZE;
+    
+    return intersect;
+  }
+  
 }
